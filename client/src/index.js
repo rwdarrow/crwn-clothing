8f828cdbdc084f46ce9f72bd88049e24fce967ae
Robbie Darrow <rwdarrow4582@eagle.fgcu.edu>
2020-05-23 00:17:41 -0400

    updated code to be PWA compliant, added serviceworker, https, and updated manifest

--- client/src/index.js
+++ client/src/index.js
@@ -5,17 +5,20 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 import { store, persistor } from "./redux/store";
+import * as serviceWorker from "./serviceWorker";
 
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
-      </BrowserRouter>
-    </Provider>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
+
+serviceWorker.register();
